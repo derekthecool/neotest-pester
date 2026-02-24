723a1f96f0f642d18140fbda9fca17438f23a4ff
Derek Lomax <derekthecool@gmail.com>
2026-02-24 09:46:53 -0700

    test: added commented out attempts at async function testing

--- tests/test_neotest-pester_core_functionality.lua
+++ tests/test_neotest-pester_core_functionality.lua
@@ -74,4 +74,37 @@ T["interface.results"] = function()
   eq("function", type(plugin.results))
 end
 
+-- Major problems running these async tests
+-- T["nio.async_run"] = function()
+--   local nio = require("nio")
+--   local task = nio.run(function()
+--     nio.sleep(10)
+--     print("Hello world")
+--     local first = nio.process.run({
+--       cmd = "printf",
+--       args = { "hello" },
+--     })
+--     local output = first.stdout.read()
+--     print(output)
+--     eq("hi", output)
+--   end)
+-- end
+
+-- T["pester.version_check"] = function()
+--   local pester_version_check =
+--     "(Get-Module -ListAvailable Pester | Sort-Object Version | Select-Object -Last 1 | Select-Object -ExpandProperty Version | Out-String).Trim()"
+--
+--   local nio = require("nio")
+--
+--   local task = nio.run(function()
+--     local pester_check =
+--       [[-NoProfile -Command '(Get-Module -ListAvailable Pester | Sort-Object Version | Select-Object -Last 1 | Select-Object -ExpandProperty Version).ToString()']]
+--     local result = nio.process.run({ cmd = "pwsh", args = pester_check })
+--     local check = result.stdout.read()
+--     print(check)
+--     eq("5.7.2", output)
+--     eq("hello", check)
+--   end)
+-- end
+
 return T
